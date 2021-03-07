ca7fbfa00336c0737deacb1ffa1aa2f80f90ac71
leo <1178472868@qq.com>
2021-03-07 14:06:36 +0800

    Mar 7th, first commit, sample for thread pool

---
+++ samples/test_thread_pool.py
@@ -0,0 +1,21 @@
+import time
+# import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+import threading
+
+
+def test(value1, value2=None):
+    print("%s threading is printed %s, %s" % (threading.current_thread().name, value1, value2))
+    time.sleep(2)
+    return 'finished'
+
+
+# def test_result(future):
+#     print(future.result())
+
+
+if __name__ == "__main__":
+    threadPool = ThreadPoolExecutor(max_workers=4, thread_name_prefix="test_")
+    for i in range(0, 10):
+        future = threadPool.submit(test, i, i+1)
+    threadPool.shutdown(wait=True)
